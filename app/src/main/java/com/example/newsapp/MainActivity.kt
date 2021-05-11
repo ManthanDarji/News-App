b7bc686205f65b1dc86ea185051a473cee9a3eda
manthan <manthandarji01@gmail.com>
2021-05-11 13:07:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.newsapp
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), NewsItemClicked {
+
+    private lateinit var mAdapter: NewsListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //we need to set the layout of recyclerview (targeted with the use of id)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        fetchData()
+        mAdapter = NewsListAdapter(this)
+        recyclerView.adapter = mAdapter
+
+
+    }
+
+    private fun fetchData(){
+
+
+        val url = "https://saurav.tech/NewsAPI/top-headlines/category/health/in.json"
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET,
+            url, null,
+            Response.Listener {
+                val newsJsonArray = it.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for(i in 0 until newsJsonArray.length()){
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+                }
+                mAdapter.updateNews(newsArray)
+
+            },
+            Response.ErrorListener {
+                    Toast.makeText(this,"Something went wrong!",Toast.LENGTH_LONG).show()
+            }
+        )
+
+// Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+
+    override fun onItemClicked(item: News) {
+        val builder =  CustomTabsIntent.Builder()
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(this, Uri.parse(item.url))
+    }
+
+
+
+}
